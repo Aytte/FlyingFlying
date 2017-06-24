726625714c189c34e2d510abad75484e783806f1
Flying <bumin35@naver.com>
2017-06-24 21:46:40 +0900

    refactored structer
    .

--- app/src/main/java/com/example/bumin/springball/Ball.java
+++ app/src/main/java/com/example/bumin/springball/Ball.java
@@ -1,6 +1,7 @@
 package com.example.bumin.springball;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,12 +15,21 @@ public class Ball extends AppCompatImageView{
     final int X = 0;
     final int Y = 1;
 
-    float acc[] = new float[]{0,0};
-    float velo[] = new float[]{0,0};
-    float loc[] = new float[]{0,0};
+    private float acc[] = new float[]{0,0};
+    public float velo[] = new float[]{0,0};
+    public float loc[] = new float[]{0,0};
 
     public Ball(Context context) {
         super(context);
+        setImageResource(R.drawable.ball);
+        setMaxHeight(120);
+        setMinimumHeight(120);
+        setMaxWidth(120);
+        setMinimumWidth(120);
+        setX(0);
+        setY(0);
+        invalidate();
+        System.out.println("Called by context");
     }
 
     public Ball(Context context, AttributeSet attrs) {
@@ -30,12 +40,67 @@ public class Ball extends AppCompatImageView{
         return loc[X] + this.getWidth()/2;
     }
 
+    public void setAcc(float x, float y){
+        this.acc[X] = x;
+        this.acc[Y] = y;
+    }
+
+    public void addAcc(float x, float y){
+        this.acc[X] += x;
+        this.acc[Y] += y;
+    }
+
+    public float getAccX(){
+        return this.acc[X];
+    }
+
+    public float getAccY(){
+        return this.acc[Y];
+    }
+
+    public void setVelo(float x, float y){
+        this.velo[X] = x;
+        this.velo[Y] = y;
+    }
+
+    public void addVelo(float x, float y){
+        this.velo[X] += x;
+        this.velo[Y] += y;
+    }
+
+    public float getVeloX(){
+        return this.velo[X];
+    }
+
+    public float getVeloY(){
+        return this.velo[Y];
+    }
+
+    public void setLoc(float x, float y){
+        this.loc[X] = x;
+        this.loc[Y] = y;
+    }
+
+    public void addLoc(float x, float y){
+        this.loc[X] += x;
+        this.loc[Y] += y;
+    }
+
+    public float getLocX(){
+        return this.loc[X];
+    }
+
+    public float getLocY(){
+        return this.loc[Y];
+    }
+
     private void initQuantity(){
         acc = new float[]{0,0};
         velo = new float[]{0,0};
         loc = new float[]{0,0};
     }
 
+    /*
     public void onTouched(View v) {
         PlayView n = (PlayView) v;
         if (isGameON) {
@@ -45,5 +110,5 @@ public class Ball extends AppCompatImageView{
         } else {
             initQuantity();
         }
-    }
+    }*/
 }
--- app/src/main/java/com/example/bumin/springball/Stage.java
+++ app/src/main/java/com/example/bumin/springball/Stage.java
@@ -3,42 +3,18 @@ package com.example.bumin.springball;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Stage extends AppCompatActivity {
+    ArrayList<Ball> springs = new ArrayList<Ball>();
+
     final float GRAVITY = (float) 0.03;
     final int X = 0;
     final int Y = 1;
-
-    Vector<Float> acc = new Vector<>();
-    Vector<Float> velo = new Vector<>();
-    Vector<Float> loc = new Vector<>();
-    Vector<Float> mousePos = new Vector<>();
-
-    ImageView ball;
-
-
-    void initQuantity() {
-        try {
-            acc.set(X, (float) 0);
-            acc.set(Y, GRAVITY);
-            velo.set(X, (float) 0);
-            velo.set(Y, (float) 0);
-            loc.set(X, ball.getX());
-            loc.set(Y, ball.getY());
-        } catch (IndexOutOfBoundsException e) {
-            acc.add((float) 0);
-            acc.add(GRAVITY);
-            velo.add((float) 0);
-            velo.add((float) 0);
-            loc.add(ball.getX());
-            loc.add(ball.getY());
-            mousePos.add((float) 0);
-            mousePos.add((float) 0);
-        }
-    }
 
     float getCenterX(View v) {
         return v.getWidth() / 2;
@@ -50,57 +26,80 @@ public class Stage extends AppCompatActivity {
     }
 */
 
-    Thread physicer = new Thread(new GravityMaker());
+    Thread phyMaker = new Thread(new Physics());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage);
-        ball = (ImageView) findViewById(R.id.ball);
-
+        springs.add(new Ball(this));
         /*
         PlayView play = new PlayView(this);
         setContentView(play);
         */
+
+        FrameLayout fram = (FrameLayout)findViewById(R.id.Frames);
+        fram.addView(springs.get(0));
     }
 
     boolean isGameON = false;
 
     public void deb(View v) {
         isGameON = false;
-        ball.setY(30);
-        ball.setX(50);
+        springs.get(0).setY(30);
+        springs.get(0).setX(50);
+        isGameON = true;
+        phyMaker.start();
+        System.out.println("button called");
     }
 
-    class GravityMaker implements Runnable {
-        void adjustAcc() {
-            if (acc.get(Y) < GRAVITY) {
-                acc.set(Y, acc.get(Y) + (float) 0.005);
+    class Physics implements Runnable {
+        float gravity = GRAVITY;
+
+        void setGravityAcc() {
+            for (Ball ball : springs) {
+                ball.setAcc(0, gravity);
             }
-            //System.out.println(acc.get(Y));
         }
 
         void addVelo() {
-            velo.set(X, velo.get(X) + acc.get(X));
+ /*           velo.set(X, velo.get(X) + acc.get(X));
             velo.set(Y, velo.get(Y) + acc.get(Y));
-            //System.out.println(velo.get(Y));
+            //System.out.println(velo.get(Y));*/
+
+            for (Ball ball : springs) {
+                ball.addVelo(ball.getAccX(), ball.getAccY());
+            }
         }
 
         void locateBall() {
-            loc.set(X, loc.get(X) + velo.get(X));
-            loc.set(Y, loc.get(Y) + velo.get(Y));
-            ball.setX(loc.get(X));
+/*            loc.set(X, loc.get(X) + velo.get(X));
+            loc.set(Y, loc.get(Y) + velo.get(Y));*/
+
+            for (Ball ball : springs) {
+                ball.setLoc(ball.getX() + ball.getVeloX(), ball.getY() + ball.getVeloY());
+                ball.setX(ball.getLocX());
+                ball.setY(ball.getLocY());
+                if (ball.getX() < 0) {
+                    ball.setVelo(-(ball.getVeloX()), ball.getVeloY());
+                }
+                if (ball.getY() < 0) {
+                    ball.setVelo(ball.getVeloX(), -(ball.getVeloY()));
+                }
+            }
+
+/*            ball.setX(loc.get(X));
             ball.setY(loc.get(Y));
             if (loc.get(X) < 0) {
                 velo.set(X, -(velo.get(X)));
             }
             if (loc.get(Y) < 0) {
                 velo.set(Y, -(velo.get(Y)));
-            }
+            }*/
         }
 
         void updatePos() {
-            adjustAcc();
+            setGravityAcc();
             addVelo();
             locateBall();
             //System.out.println("Complete");
@@ -119,3 +118,4 @@ public class Stage extends AppCompatActivity {
         }
     }
 }
+
